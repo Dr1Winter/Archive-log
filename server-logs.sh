eecb32035eaff8940ea09acfc51e33a303ff3ca2
DrWinter <werty2951329513@gmail.com>
2025-01-04 22:05:36 +0300

    Alpha 1.0

---
+++ server-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+NAME=$(date +%Y-%m-%d-%H-%M-%S)
+OUTPUTHLOGTAR="/home/Server-logs"
+PATHTOLOGTAR="/var/log"
+while [ -n "$1" ]
+do 
+case "$1" in
+-p) PATHTOLOGTAR="$2"
+shift;;
+-o) OUTPUTHLOGTAR="2";;
+esac
+shift 
+done
+sudo mkdir -p $OUTPUTHLOGTAR
+sudo tar -cvzf server-logs-"$NAME".tar.gz "$PATHTOLOGTAR"
+sudo mv server-logs-"$NAME".tar.gz /home/Server-logs
