97e896b8b8c775fa0e9a921e6ea120238c88b2bd
DrWinter <werty2951329513@gmail.com>
2025-01-05 15:09:12 +0300

    Add instalation

--- server-logs.sh
+++ server-logs.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 NAME=$(date +%Y-%m-%d-%H-%M-%S)
-OUTPUTHLOGTAR="/home/Server-logs"
+OUTPUTHLOGTAR="/home/server-logs"
 PATHTOLOGTAR="/var/log"
+CURENTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while [ -n "$1" ]
 do 
 case "$1" in
--p) PATHTOLOGTAR="$2"
+-p | --path) PATHTOLOGTAR="$2"
 shift;;
--o) OUTPUTHLOGTAR="$2";;
+-o | --outputpath) OUTPUTHLOGTAR="$2";;
+-i |--install) sudo cp -r $CURENTDIR /opt/
+sudo cp /opt/application/application.desktop /home/$USER/.local/share/applications
+chmod +x /home/$USER/.local/share/applications/application.desktop;;
 esac
 shift 
 done
