1674dc420ad7f21cb17e0c9a27153223f212a2f8
DrWinter <werty2951329513@gmail.com>
2025-01-06 09:11:16 +0300

    Add time

--- server-logs.sh
+++ server-logs.sh
@@ -3,18 +3,34 @@ NAME=$(date +%Y-%m-%d-%H-%M-%S)
 OUTPUTHLOGTAR="/home/server-logs"
 PATHTOLOGTAR="/var/log"
 CURENTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+mkdir -p $OUTPUTHLOGTAR
 while [ -n "$1" ]
 do 
 case "$1" in
 -p | --path) PATHTOLOGTAR="$2"
 shift;;
--o | --outputpath) OUTPUTHLOGTAR="$2";;
--i |--install) sudo cp -r $CURENTDIR /opt/
-sudo cp /opt/application/application.desktop /home/$USER/.local/share/applications
-chmod +x /home/$USER/.local/share/applications/application.desktop;;
+-o | --outputpath) OUTPUTHLOGTAR="$2"
+shift;;
+-t |--time)
+case "$2" in
+m | minute) ROPTIONS="* * * * *";;
+h | hour) ROPTIONS="0 * * * *";;
+d | day) ROPTIONS="0 0 * * *";;
+mo | month) ROPTIONS="0 0 1 * *";;
+y | year) ROPTIONS="0 0 1 12 *";;
+*) echo "Invalyd syntax"
+break;;
+esac 
+shift
+touch newcron
+crontab -l > newcron
+echo "$ROPTIONS" " " "$CURENTDIR" >> newcron
+crontab newcron
+rm newcron;;
+*) echo "Invalyd syntax"
+break;;
 esac
 shift 
 done
-sudo mkdir -p $OUTPUTHLOGTAR
-sudo tar -cvzf server-logs-"$NAME".tar.gz "$PATHTOLOGTAR"
-sudo mv server-logs-"$NAME".tar.gz $OUTPUTHLOGTAR
+tar -cvzf server-logs-"$NAME".tar.gz "$PATHTOLOGTAR" > dev/null
+mv server-logs-"$NAME".tar.gz $OUTPUTHLOGTAR
